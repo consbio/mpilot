15e14d2ba1ff1f601a42fd8731bd64452ad2ddef
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2021-02-24 17:48:22 -0800

    Add CLI option for adding custom libraries

--- mpilot/cli/mpilot.py
+++ mpilot/cli/mpilot.py
@@ -16,12 +16,22 @@ LINE_CONTEX_LENGTH = 3
 @click.command()
 @click.argument("library")
 @click.argument("path")
-def main(library, path):
+@click.option(
+    "--library",
+    "-l",
+    "libraries",
+    multiple=True,
+    default=[],
+    help="Add a command library by its Python path (e.g., mpilot.libraries.eems.csv)",
+)
+def main(library, path, libraries):
     if not os.path.exists(path):
         sys.stderr.write(
             "\n".join(
                 (
-                    "Problem: The specified command file does not exist: {}".format(path),
+                    "Problem: The specified command file does not exist: {}".format(
+                        path
+                    ),
                     "Solution: Check the path to the command file and try again.",
                 )
             )
@@ -39,11 +49,21 @@ def main(library, path):
     elif library == "eems-netcdf":
         Command.load_commands("mpilot.libraries.eems.netcdf")
 
+    for lib in libraries:
+        Command.load_commands(lib)
+
     try:
         program = Program.from_source(source)
         program.run()
     except MPilotError as ex:
-        sys.stderr.write("\n".join(("ERROR: There was a problem running the MPilot command file.", six.text_type(ex))))
+        sys.stderr.write(
+            "\n".join(
+                (
+                    "ERROR: There was a problem running the MPilot command file.",
+                    six.text_type(ex),
+                )
+            )
+        )
         sys.stderr.write("\n")
 
         if isinstance(ex, ProgramError) and ex.lineno is not None:
@@ -55,7 +75,9 @@ def main(library, path):
             sys.stderr.write("\n")
             sys.stderr.write("--> {}".format(lines[idx]))
             sys.stderr.write("\n")
-            sys.stderr.write("\n".join((" " * 4 + line for line in lines[idx + 1 : end])))
+            sys.stderr.write(
+                "\n".join((" " * 4 + line for line in lines[idx + 1 : end]))
+            )
             sys.stderr.write("\n")
 
         sys.exit(-1)
